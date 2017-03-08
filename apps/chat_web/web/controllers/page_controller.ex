6908f967f0a949798adcacb5c4f956bfafe632f8
Rage Gage <gnewman@appacademy.io>
2017-03-07 16:31:17 -0800

    hello world from the chat server!

--- apps/chat_web/web/controllers/page_controller.ex
+++ apps/chat_web/web/controllers/page_controller.ex
@@ -2,6 +2,14 @@ defmodule ChatWeb.PageController do
   use ChatWeb.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    ChatServer.Supervisor.start_link
+    ChatServer.Supervisor.start_room("gage")
+    ChatServer.create("gage", "hello world")
+    [chat | t] = ChatServer.get("gage")
+    IO.inspect chat
+    render conn, 
+      "index.html", 
+      content: chat.content,
+      username: chat.username
   end
 end
