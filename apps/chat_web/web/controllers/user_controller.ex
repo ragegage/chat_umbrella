71b12d7348872a84d5d36e0c3e93575e3012c8b9
Rage Gage <gnewman@appacademy.io>
2017-03-21 13:49:18 -0700

    add create action to usercontroller

--- apps/chat_web/web/controllers/user_controller.ex
+++ apps/chat_web/web/controllers/user_controller.ex
@@ -2,6 +2,8 @@ defmodule ChatWeb.UserController do
   use ChatWeb.Web, :controller
 
   alias ChatWeb.User
+  
+  plug :scrub_params, "user" when action in [:create]
 
   def show(conn, %{"id" => id}) do
     user = Repo.get!(User, id)
@@ -13,6 +15,16 @@ defmodule ChatWeb.UserController do
     render conn, "new.html", changeset: changeset
   end
 
+
   def create(conn, %{"user" => user_params}) do
+    changeset = %User{} |> User.registration_changeset(user_params)
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "#{user.email} created!")
+        |> redirect(to: user_path(conn, :show, user))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
   end
 end
