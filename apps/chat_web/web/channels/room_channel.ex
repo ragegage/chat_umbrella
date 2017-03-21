bacd2435299a1464349f0a76a020879575d99e1b
Rage Gage <gnewman@appacademy.io>
2017-03-20 17:57:26 -0700

    add IO.inspects everywhere

--- apps/chat_web/web/channels/room_channel.ex
+++ apps/chat_web/web/channels/room_channel.ex
@@ -4,17 +4,17 @@ defmodule ChatWeb.RoomChannel do
   use Phoenix.Channel
 
   def join("room:" <> room_name, _message, socket) do
-    ChatServer.Supervisor.start_link
-    ChatServer.Supervisor.start_room(room_name)
-    chat_list = ChatServer.get(room_name)
+    ChatServer.Supervisor.start_link |> IO.inspect
+    ChatServer.Supervisor.start_link |> IO.inspect
+    ChatServer.Supervisor.start_room(room_name) |> IO.inspect
+    chat_list = ChatServer.get(room_name) |> IO.inspect
     {:ok, chat_list, socket}
   end
 
   def handle_in("new_msg", %{"body" => body}, socket) do 
     "room:" <> room_name = socket.topic
     ChatServer.create(room_name, body)
-    IO.puts "new message coming in saying #{body}"
-    IO.inspect socket
+    ChatServer.get(room_name) |> IO.inspect
     broadcast! socket, "new_msg", %{body: body}
     {:noreply, socket} 
   end
