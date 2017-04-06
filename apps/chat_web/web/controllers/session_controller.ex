c5a37ca4ac58a52778e1ca4222fa098d832be2d8
Gage Newman <ragegage@Gages-MacBook-Pro.local>
2017-04-05 22:10:09 -0700

    fix session creation when no email is passed

--- apps/chat_web/web/controllers/session_controller.ex
+++ apps/chat_web/web/controllers/session_controller.ex
@@ -1,3 +1,5 @@
+require IEx
+
 defmodule ChatWeb.SessionController do
   use ChatWeb.Web, :controller
   plug :scrub_params, "session" when action in ~w(create)a
@@ -6,17 +8,25 @@ defmodule ChatWeb.SessionController do
   end
   def create(conn, %{"session" => %{"email" => email,
                                     "password" => password}}) do
-    case ChatWeb.Auth.login_by_email_and_pass(conn, email,
-                                                 password) do
-      {:ok, conn} ->
-        conn
-        # |> put_flash(:info, "You’re now signed in!")
-        |> redirect(to: page_path(conn, :index))
-      {:error, _reason, conn} ->
+    # IEx.pry
+    case email do
+      nil ->
         conn
         |> put_flash(:error, "Invalid email/password combination")
         |> render("new.html")
-     end
+      _ ->
+        case ChatWeb.Auth.login_by_email_and_pass(conn, email,
+                                                     password) do
+          {:ok, conn} ->
+            conn
+            # |> put_flash(:info, "You’re now signed in!")
+            |> redirect(to: page_path(conn, :index))
+          {:error, _reason, conn} ->
+            conn
+            |> put_flash(:error, "Invalid email/password combination")
+            |> render("new.html")
+        end
+    end
   end
   def delete(conn, _) do
     conn
